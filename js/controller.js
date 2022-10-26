8a066ea572c13f25176f089bab9770f5fb08da77
NeiValHein <37918247+NeiValHein@users.noreply.github.com>
2022-10-25 18:19:47 -0600

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -29,29 +29,28 @@ window.stationMD.functions.PWAController = function PWAController() {
 	});
 	setTimeout(() => {
 		if (!deferredPrompt && localStorage.getItem('event_fired') == 'true' && !ios || window.matchMedia('(display-mode: standalone)').matches || ios || firefoxBrowser || operaBrowser || safariBrowser) {
-			console.log('main conditional false');
 			window.location.replace("https://connect.stationmd.com/zoom-token");
 		} else {
 			let container = document.getElementById('container');
 			let html = document.createElement('div');
-			html.innerHTML = `<img src="/stationMD/img/assets/logo.png" class="img-fluid items-header" alt="StationMD Logo">
+			html.innerHTML = `<img src="/wp-content/img/assets/logo.png" class="img-fluid items-header" alt="StationMD Logo">
 			<div class="container">
 			<div class="row">
 			<div class="column-0">
 			<div class="row">
 			<div class="item" id="pwa-card">
-			<img src="/stationMD/img/assets/browsers.png" class="big-img img-fluid" alt="Chrome Logo">
+			<img src="/wp-content/img/assets/browsers.png" class="big-img img-fluid" alt="Chrome Logo">
 			<h2> StationMD Desktop / Laptop Application</h2>
 			<p id="pwa-message">To install this application on to your desktop or laptop device, simply click on the <br><strong> ADD TO HOME SCREEN </strong> button below and then click <strong>Install</strong>.</p>
 			<button class="pwa-btn install-btn" id="pwa-install">ADD TO HOME SCREEN</button>
 			</div>
 			<div class="item-divider"></div>
 			<div class="item">
-			<img src="/stationMD/img/assets/qr.png" class="big-img img-fluid" alt="QR Code">
+			<img src="/wp-content/img/assets/qr.png" class="big-img img-fluid" alt="QR Code">
 			<h2>StationMD Mobile Application</h2>
 			<p>Open the built-in camera app. Point the camera at the QR code. Tap the banner that appears on your Android or iOS device. Follow the instructions on the screen to finish installation.</p>
-			<a href="https://play.google.com/store/apps/details?id=com.stationmd.stationmd&gl=US"><img src="/stationMD/img/assets/play-store.png" class="small-img img-fluid margin-r-5" alt="get the mobile application by scanning the QR code"></a>
-			<a href="https://apps.apple.com/us/app/stationmd/id1476404286"><img src="/stationMD/img/assets/app-store.png" class="small-img img-fluid" alt="get the mobile application by scanning the QR code"></a>
+			<a href="https://play.google.com/store/apps/details?id=com.stationmd.stationmd&gl=US"><img src="/wp-content/img/assets/play-store.png" class="small-img img-fluid margin-r-5" alt="get the mobile application by scanning the QR code"></a>
+			<a href="https://apps.apple.com/us/app/stationmd/id1476404286"><img src="/wp-content/img/assets/app-store.png" class="small-img img-fluid" alt="get the mobile application by scanning the QR code"></a>
 			</div>
 			</div>
 			</div>
@@ -88,21 +87,14 @@ window.stationMD.functions.PWAController = function PWAController() {
 					deferredPrompt.prompt();
 					const { outcome } = await deferredPrompt.userChoice;
 					if (outcome === 'accepted') {
-						if (!android || !ios) {
-							console.log('both conditionals are false')
-							if (!edgeBrowser) {
-								window.location.replace("https://connect.stationmd.com/zoom-token");
-							} else if (macOS) {
-								window.location.replace("https://connect.stationmd.com/zoom-token");
-							}						
-						} else if (android) {
-							console.log('android conditional are true');
-
-							window.location.replace("https://play.google.com/store/apps/details?id=com.stationmd.stationmd&gl=US");
-						} else if (ios) {
-							console.log('ios conditional are true');
-
-							window.location.replace("https://apps.apple.com/us/app/stationmd/id1476404286");	
+						if (!edgeBrowser) {
+							console.log('no edge')
+							window.location.replace("https://connect.stationmd.com/zoom-token");
+						} else if (maOS) {
+							console.log('mac detected')
+							window.location.replace("https://connect.stationmd.com/zoom-token");
+						} else {
+							console.log('both false')
 						}
 						deferredPrompt = null;
 						installMsg.innerHTML = 'Thank you for installing our pwa.';
